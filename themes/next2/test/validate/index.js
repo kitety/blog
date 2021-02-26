cf5a1fee2528e7cf8e79df7ef58f0bb1a36b1be0
念歌 <niange@qunhemail.com>
2021-02-26 14:41:19 +0800

    feat: change gulpfile

--- themes/next2/test/validate/index.js
+++ themes/next2/test/validate/index.js
@@ -1,33 +1,38 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
-const should = require('chai').should();
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
+const should = require("chai").should();
 
-describe('Validate', () => {
-  it('config', () => {
-    const themeConfig = fs.readFileSync(path.join(__dirname, '../../_config.yml'));
+const yamlLoad = yaml.load || yaml.safeLoad;
+describe("Validate", () => {
+  it("config", () => {
+    const themeConfig = fs.readFileSync(
+      path.join(__dirname, "../../_config.yml")
+    );
     should.not.throw(() => {
-      yaml.safeLoad(themeConfig);
+      yamlLoad(themeConfig);
     });
   });
 
-  it('vendors', () => {
-    const vendorsFile = fs.readFileSync(path.join(__dirname, '../../_vendors.yml'));
+  it("vendors", () => {
+    const vendorsFile = fs.readFileSync(
+      path.join(__dirname, "../../_vendors.yml")
+    );
     should.not.throw(() => {
-      yaml.safeLoad(vendorsFile);
+      yamlLoad(vendorsFile);
     });
   });
 
-  it('language', () => {
-    const languagesPath = path.join(__dirname, '../../languages');
+  it("language", () => {
+    const languagesPath = path.join(__dirname, "../../languages");
     should.not.throw(() => {
-      fs.readdirSync(languagesPath).forEach(lang => {
-        if (!lang.endsWith('.yml')) return;
+      fs.readdirSync(languagesPath).forEach((lang) => {
+        if (!lang.endsWith(".yml")) return;
         const languagePath = path.join(languagesPath, lang);
-        yaml.safeLoad(fs.readFileSync(languagePath), {
-          filename: path.relative(__dirname, languagePath)
+        yamlLoad(fs.readFileSync(languagePath), {
+          filename: path.relative(__dirname, languagePath),
         });
       });
     });
